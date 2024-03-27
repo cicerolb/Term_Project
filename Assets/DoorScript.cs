8f041deda022d52b68106de81da5ba032d63fe5a
cicerolb <115563856+cicerolb@users.noreply.github.com>
2024-03-27 17:29:46 +0800

    asdasd

---
+++ Assets/DoorScript.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+    Transform selectedDoor;
+    GameObject dragPointGameobject;
+    int leftDoor = 0;
+    [SerializeField] LayerMask doorLayer;
+
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, 20, doorLayer))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                selectedDoor = hit.collider.gameObject.transform;
+            }
+        }
+
+        if (selectedDoor != null)
+        {
+            HingeJoint joint = selectedDoor.GetComponent<HingeJoint>();
+            JointMotor motor = joint.motor;
+
+            if (dragPointGameobject == null)
+            {
+                dragPointGameobject = new GameObject("Ray door");
+                dragPointGameobject.transform.parent = selectedDoor;
+            }
+
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            dragPointGameobject.transform.position = ray.GetPoint(Vector3.Distance(selectedDoor.position, transform.position));
+            dragPointGameobject.transform.rotation = selectedDoor.rotation;
+
+            float delta = Mathf.Pow(Vector3.Distance(dragPointGameobject.transform.position, selectedDoor.position), 3);
+
+            if (selectedDoor.GetComponent<MeshRenderer>().localBounds.center.x > selectedDoor.localPosition.x)
+            {
+                leftDoor = 1;
+            }
+            else
+            {
+                leftDoor = -1;
+            }
+
+            float speedMultiplier = 60000;
+            if (Mathf.Abs(selectedDoor.parent.forward.z) > selectedDoor.position.x)
+            {
+                if (dragPointGameobject.transform.position.x > selectedDoor.position.x)
+                {
+                    motor.targetVelocity = delta * -speedMultiplier * Time.deltaTime * leftDoor;
+                }
+                else
+                {
+                    motor.targetVelocity = delta * speedMultiplier * Time.deltaTime * leftDoor;
+                }
+            }
+            else
+            {
+                if (dragPointGameobject.transform.position.z > selectedDoor.position.z)
+                {
+                    motor.targetVelocity = delta * -speedMultiplier * Time.deltaTime * leftDoor;
+                }
+                else
+                {
+                    motor.targetVelocity = delta * speedMultiplier * Time.deltaTime * leftDoor;
+                }
+            }
+            joint.motor = motor;
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                selectedDoor = null;
+                motor.targetVelocity = 0;
+                joint.motor = motor;
+                Destroy(dragPointGameobject);
+            }
+        }
+    }
+}
+
